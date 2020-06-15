c069d0735f810cf03ffe3b9f41ba3483d8483e92
Aditya-sairam <37501823+Aditya-sairam@users.noreply.github.com>
2020-06-15 10:05:37 +0530

    length of last word

---
+++ lastwordlength.py
@@ -0,0 +1,16 @@
+class lastwordlength:
+    def lengthOfLastWord(self, s: str) -> int: 
+        length = 0 
+        
+        x = s.strip() 
+        
+        for i in range(len(x)):
+            if x[i] == " ":
+                length = 0 
+            else:
+                length += 1 
+                
+                
+        return length
+        
+            
